5ee7a05412a0b199f99ea40e2cddf2709eda406c
talomina <76668907+talomina@users.noreply.github.com>
2024-02-12 21:44:23 +0900

    Update start-script.sh

--- start-script.sh
+++ start-script.sh
@@ -3,8 +3,10 @@ sudo apt-get update
 sudo apt-get install -y default-jre-headless
 sudo apt-get install wget
 sudo apt-get install -y screen
-
 sudo mkdir -p /home/minecraft
 cd /home/minecraft
 sudo wget https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar
 sudo screen -S mcs java -Xmx1024M -Xms1024M -jar server.jar nogui
+sudo sh -c "echo eula=true > eula.txt"
+# sudo screen -S mcs java -Xmx1024M -Xms1024M -jar server.jar nogui
+# sudo screen -S mcs java -Xms1G -Xmx1G -jar server.jar nogui
